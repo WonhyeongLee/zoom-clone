cc7e13680ecf856f154c079972f06b90a83a945f
WonhyeongLee <bigfile57@naver.com>
2023-02-21 18:19:16 +0900

    :sparkles: 사용자가 어떤 닉네임으로 채팅하는지 보여주도록 함
    지금까지 코드는  WebSockets 로 구현했지만 SocketIO로 바꿀 예정

--- src/server.js
+++ src/server.js
@@ -13,19 +13,39 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 // app.listen(3000, handleListen);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
+const sockets = [];
 // 같은 서버에 http, webSocket 둘 다 작동시키는 형태
 
-function onSubmitMessage(message) {
-  sockets.forEach((socket) => socket.send(message.toString()));
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
 }
 
-const sockets = [];
+function onSubmitMessage(msg, socket) {
+  const message = JSON.parse(msg);
+  switch (message.type) {
+    case "new_message":
+      console.log(socket.nickname, message.payload);
+      sockets.forEach((aSocket) =>
+        aSocket.send(makeMessage("new_message", `${socket.nickname}: ${message.payload}`))
+      );
+      break;
+    case "nickname":
+      socket["nickname"] = message.payload;
+      socket.send(makeMessage("nickname", socket.nickname));
+      break;
+  }
+}
 
 wss.on("connection", (socket) => {
   sockets.push(socket);
+  socket["nickname"] = "익명";
+  socket.send(makeMessage("nickname", socket.nickname));
   console.log("Connected to Server");
-  socket.on("close", () => console.log("Disconnected from the Browser"));
-  socket.on("message", onSubmitMessage);
-  socket.send("hello!!");
+  socket.on("close", () => {
+    console.log("Disconnected from the Browser");
+    // sockets.splice(sockets.indexOf(socket), 1);
+  });
+  socket.on("message", (msg) => onSubmitMessage(msg, socket));
 });
 server.listen(3000, handleListen);
